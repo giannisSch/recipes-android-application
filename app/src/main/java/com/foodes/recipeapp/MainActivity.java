002b77574e50954e9c3bbc2b21bb4aac07c1c621
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-05-24 12:57:13 +0300

    24/5 | Splash Screen, Buttons functionality

--- app/src/main/java/com/foodes/recipeapp/MainActivity.java
+++ app/src/main/java/com/foodes/recipeapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.foodes.recipeapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, GeneralScreen.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
+
 }
