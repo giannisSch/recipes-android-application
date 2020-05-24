1781af0a4f3091be781c689c85245575aa9ea2db
giannisSch <g.sxoin@hotmail.com>
2020-05-24 10:38:47 +0300

    init

--- app/src/main/java/com/foodes/recipeapp/MainActivity.java
+++ app/src/main/java/com/foodes/recipeapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.foodes.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        Button loginButton = findViewById(R.id.mainLoginButton);
+        Button createAnAccountButton = findViewById(R.id.mainCreateAnAccountButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoActivity(LoginActivity.class);
+            }
+        });
+        createAnAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoActivity(SignUpActivity.class);
+            }
+        });
+    }
+    private void gotoActivity(Class activityName){
+        Intent intent = new Intent(MainActivity.this, activityName);
+        startActivity(intent);
     }
 
 }
