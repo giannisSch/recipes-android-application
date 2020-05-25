91e7bf4f76e7f16ce57db28d164a525119dac63a
giannisSch <g.sxoin@hotmail.com>
2020-05-25 21:32:08 +0300

    init

--- app/src/main/java/com/foodes/recipeapp/SplashActivity.java
+++ app/src/main/java/com/foodes/recipeapp/SplashActivity.java
@@ -1,14 +1,28 @@
 package com.foodes.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
