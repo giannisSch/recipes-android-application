4e0af68b6ec1f7c40e4a19a27d7d805f7caeaf8b
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-06-05 00:38:04 +0300

    4/6 | Drawer v.3 functionality OK

--- app/src/main/java/com/foodes/recipeapp/drawer_test.java
+++ app/src/main/java/com/foodes/recipeapp/drawer_test.java
@@ -2,32 +2,52 @@ package com.foodes.recipeapp;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
 
 public class drawer_test extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar;
-    Menu menu;
-
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_test);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.nav_view);
 
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //
+                return true;
+            }
+        });
     }
+
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
