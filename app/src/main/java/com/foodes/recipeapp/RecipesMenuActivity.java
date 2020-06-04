7f1864c330f71f3ae1305a7328cd7f8063b1be87
giannisSch <g.sxoin@hotmail.com>
2020-06-04 22:51:22 +0300

    RecyclerView Recipe Details

--- app/src/main/java/com/foodes/recipeapp/RecipesMenuActivity.java
+++ app/src/main/java/com/foodes/recipeapp/RecipesMenuActivity.java
@@ -52,11 +52,11 @@ public class RecipesMenuActivity extends AppCompatActivity {
         });
         recyclerView.setAdapter(adapter);
 
- //       Intent intent = getIntent();
-//        String ingredient = intent.getExtras().getString("ingredient");
+        //Intent intent = getIntent();
+        //String ingredient = intent.getExtras().getString("ingredient");
 
         RequestQueue queue = Volley.newRequestQueue(this);
-       // String url = "https://api.edamam.com/search?q=" + ingredient + "&app_id=57e6d292&app_key=000b571f7a78bfb4fb9820a7cc3b283e";
+        //String url = "https://api.edamam.com/search?q=" + ingredient + "&app_id=57e6d292&app_key=000b571f7a78bfb4fb9820a7cc3b283e";
         String url = "https://api.edamam.com/search?q=egg&app_id=57e6d292&app_key=000b571f7a78bfb4fb9820a7cc3b283e";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
