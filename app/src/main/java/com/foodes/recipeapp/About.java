ca4a193481b2653a7aad93fc353a3264ab2c081d
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-06-11 01:58:17 +0300

    11/6 | About page

--- app/src/main/java/com/foodes/recipeapp/About.java
+++ app/src/main/java/com/foodes/recipeapp/About.java
@@ -1,13 +1,5 @@
 package com.foodes.recipeapp;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -16,30 +8,31 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.textview.MaterialTextView;
 
-public class SearchActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class About extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private String loggedInUsername;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
-    private MaterialTextView greeting;
-    Button searchBtn;
-    private EditText basicIngredient;
     int userId;
 
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_about);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        basicIngredient = (EditText)findViewById(R.id.basicIngredientEditText);
+        toolbar.setTitle(null);
+
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.nav_view);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
@@ -50,42 +43,12 @@ public class SearchActivity extends AppCompatActivity implements NavigationView.
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        Intent getUsername = getIntent();
-        loggedInUsername = getUsername.getStringExtra("Username");
-
-        greeting = findViewById(R.id.user_greeting_txt);
-
-        //getting data from login activity
         Intent GreetLoggedInUser = getIntent();
         String username = GreetLoggedInUser.getStringExtra("Username");
-        greeting.setText("Hello" + " " + username + "!");
         userId = getIntent().getIntExtra("userId",0);
-        //drawer selection switch
         navigationView.setNavigationItemSelectedListener(this);
-
-        searchBtn = findViewById(R.id.drawerSearchBtn);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SearchActivity.this, RecipesMenuActivity.class);
-                intent.putExtra("ingredient", basicIngredient.getText().toString().toLowerCase().trim());
-                intent.putExtra("userId",userId);
-                intent.putExtra("Username", loggedInUsername);
-                startActivity(intent);
-            }
-        });
-
         toggle.syncState();
-    }
 
-    //swipe right or back button functionality
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -97,38 +60,37 @@ public class SearchActivity extends AppCompatActivity implements NavigationView.
                 askIfUserIsSure();
                 break;
             case R.id.nav_profile:
-                Intent goToProfile = new Intent(SearchActivity.this,UserProfile.class);
+                Intent goToProfile = new Intent(About.this,UserProfile.class);
                 goToProfile.putExtra("Username", loggedInUsername);
                 goToProfile.putExtra("userId", userId);
                 startActivity(goToProfile);
                 break;
             case R.id.nav_favorites:
-                Intent goToFavoriteList = new Intent(SearchActivity.this, FavoritesActivity.class);
+                Intent goToFavoriteList = new Intent(About.this, FavoritesActivity.class);
                 goToFavoriteList.putExtra("Username", loggedInUsername);
                 goToFavoriteList.putExtra("userId", userId);
                 startActivity(goToFavoriteList);
                 break;
             case R.id.nav_otherUsers:
-                Intent goToOtherUsers = new Intent(SearchActivity.this, OtherUsers.class);
+                Intent goToOtherUsers = new Intent(About.this, OtherUsers.class);
                 goToOtherUsers.putExtra("Username", loggedInUsername);
                 goToOtherUsers.putExtra("userId", userId);
                 startActivity(goToOtherUsers);
             case R.id.nav_about:
-                Intent goToAbout = new Intent(SearchActivity.this, About.class);
-                startActivity(goToAbout);
+                return true;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
     private void askIfUserIsSure(){
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(SearchActivity.this);
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(About.this);
         builder.setTitle("Logout");
         builder.setMessage("Are you sure that you want to logout?");
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Intent redirectToMainScreen = new Intent(SearchActivity.this,MainActivity.class);
+                Intent redirectToMainScreen = new Intent(About.this,MainActivity.class);
                 startActivity(redirectToMainScreen);
             }
         });
